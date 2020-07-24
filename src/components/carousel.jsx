db1bf30f6625d930ebfe7cca419d5bc9135e4d98
Alexandra <55248127+randomartlover@users.noreply.github.com>
2020-07-24 14:16:30 +0100

    carousel adjusted

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -18,7 +18,7 @@ class SimpleSlider extends React.Component {
           <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
         </ol>
         <div className="carousel-inner">
-          <div className="carousel-item">
+          <div className="carousel-item active">
             <img src={slide3} className="carousel-img" alt="First slide" />
             <div className="carousel-caption d-block d-md-block">
               <h3 className="text-center font-weight-bold">By 2020, 1 in 2 ePayment transactions will be made on our platforms</h3>
@@ -29,12 +29,12 @@ class SimpleSlider extends React.Component {
             <img className="carousel-img" src={slide2} alt="Second slide" />
             <div className="carousel-caption d-block d-md-block">
               <h2 className="Slide-4-caption"><span>Easy. </span>
-              <span>Fast. </span>
-              <span>Secure. </span></h2>
+                <span>Fast. </span>
+                <span>Secure. </span></h2>
             </div>
           </div>
 
-          <div className="carousel-item active">
+          <div className="carousel-item">
             <img className="carousel-img" src={slide3} alt="Second slide" />
             <div className="carousel-caption d-block d-md-block">
 
@@ -45,10 +45,10 @@ class SimpleSlider extends React.Component {
                   <div className="col-md-2 col-sm-2 col-xs-2"></div>
                   <button className="box1 col-md-4 col-sm-4 col-xs-4 m-2 font-weight-bold">
                     LEARN MORE
-                  </button>
+                    </button>
                   <button className="box2 col-md-4 col-sm-4 col-xs-4 m-2 font-weight-bold">
                     CONTACT US
-                  </button>
+                    </button>
                   <div className="col-md-2 col-sm-2 col-xs-2"></div>
                 </div>
               </div>
@@ -58,23 +58,24 @@ class SimpleSlider extends React.Component {
 
 
           <div className="carousel-item">
-            <img src={slide3} className="carousel-img"  alt="Third slide" />
+            <img src={slide3} className="carousel-img" alt="Third slide" />
             <Carousel.Caption className="carousel_caption"> <h3>Our purpose is to simplify and enable smarter living</h3></Carousel.Caption>
 
           </div>
         </div>
 
 
+        <div className="carousel-action-container"><a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <svg className="carousel-action-icon" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m256 512c-68.378906 0-132.667969-26.628906-181.019531-74.980469-48.351563-48.351562-74.980469-112.640625-74.980469-181.019531s26.628906-132.667969 74.980469-181.019531c48.351562-48.351563 112.640625-74.980469 181.019531-74.980469s132.667969 26.628906 181.019531 74.980469c48.351563 48.351562 74.980469 112.640625 74.980469 181.019531s-26.628906 132.667969-74.980469 181.019531c-48.351562 48.351563-112.640625 74.980469-181.019531 74.980469zm0-472c-119.101562 0-216 96.898438-216 216s96.898438 216 216 216 216-96.898438 216-216-96.898438-216-216-216zm62.285156 326-110-110 110-110-28.285156-28.285156-138.285156 138.285156 138.285156 138.285156zm0 0" /></svg>
+        </a>
 
-        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="sr-only">Next</span>
-        </a>
-      </div>
+
+          <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <svg className="carousel-action-icon" height="512pt" viewBox="0 0 512 512" width="512pt" xmlns="http://www.w3.org/2000/svg"><path d="m256 512c-68.378906 0-132.667969-26.628906-181.019531-74.980469-48.351563-48.351562-74.980469-112.640625-74.980469-181.019531s26.628906-132.667969 74.980469-181.019531c48.351562-48.351563 112.640625-74.980469 181.019531-74.980469s132.667969 26.628906 181.019531 74.980469c48.351563 48.351562 74.980469 112.640625 74.980469 181.019531s-26.628906 132.667969-74.980469 181.019531c-48.351562 48.351563-112.640625 74.980469-181.019531 74.980469zm0-472c-119.101562 0-216 96.898438-216 216s96.898438 216 216 216 216-96.898438 216-216-96.898438-216-216-216zm104.285156 216-138.285156-138.285156-28.285156 28.285156 110 110-110 110 28.285156 28.285156zm0 0" /></svg>
+            {/* <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span> */}
+          </a>
+        </div></div>
     );
   }
 }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,57 +1,34 @@
-import React from 'react';
-import logo from './Images/logo.png';
+import React, { Component } from 'react';
+import MobileNav from './MobileNav';
+import DesktopNav from './DesktopNav';
 
-const NavBar = () => {
-  return (<nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#"><img src={logo} alt="First slide" /></a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-
-    <div className="collapse navbar-collapse mr-auto" id="navbarSupportedContent">
-
-      <ul className="navbar-nav  ml-auto">
-        <li className="nav-item active">
-          <a className="nav-link navbar_link active" href="#">Home </a>
-        </li>
+class NavBar extends Component {
+  state = { navActive: false, subNavActive: false }
 
 
-        <li className="nav-item">
-          <a className="nav-link navbar_link" href="#">Link</a>
-        </li>
+  handleSubNavClick = () => {
+    if (this.state.subNavActive === false) {
+      this.setState({ subNavActive: true })
+    }
+    else { this.setState({ subNavActive: false }) }
+  }
 
 
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle navbar_link" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            About Us
-        </a>
-          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a className="dropdown-item" href="#">Our Company</a>
-            <a className="dropdown-item" href="#">Board of Directors</a>
-            <a className="dropdown-item" href="#">Management Team</a>
-            <a className="dropdown-item" href="#">Our Partners</a>
-          </div>
-        </li>
+  handleNavClick = () => {
+    if (this.state.navActive === false) {
+      this.setState({ navActive: true })
+    }
+    else { this.setState({ navActive: false }) }
 
-        <li className="nav-item">
-          <a className="nav-link" href="#">Career</a>
-        </li>
+  }
 
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            News & Events
-        </a>
-          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a className="dropdown-item" href="#">News</a>
-            <a className="dropdown-item" href="#">Events</a>
-
-          </div>
-        </li>
-
-      </ul>
-
-    </div>
-  </nav>);
+  render() {
+    const {navActive, subNavActive} = this.state;
+    return (<React.Fragment>
+      <MobileNav onNavClick={this.handleNavClick} NavOpen={navActive} onSubNavClick={this.handleSubNavClick} SubNavOpen={subNavActive}/>
+      <DesktopNav />
+    </React.Fragment>);
+  }
 }
 
 export default NavBar;
